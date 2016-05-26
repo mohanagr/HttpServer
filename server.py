fcb68dbae28a48cacc29c2c3759501559a994084
Mohan Agrawal <mohan.agr97@gmail.com>
2016-05-26 21:57:36 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import socketserver
+import socket
+from utils import response
+
+
+class MyTCPServer(socketserver.TCPServer):
+	def server_bind(self):
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind(self.server_address)
+
+class MyRequestHandler(socketserver.BaseRequestHandler, response.ResponseHandler):
+
+	def __init__(self, request, client_address, server, tclass = response.ResponseHandler):
+		self.tclass = tclass
+		response.ResponseHandler.__init__(self, request)
+		socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
+		
+
+	def handle(self):
+		# while True:
+		data = self.request.recv(1024)
+		print(data)
+		self.tclass.MASTERHANDLER(self)
+		# self.request.sendall(bytes('damn it', 'utf-8'))
+
+
+
+if __name__ == "__main__":
+	
+	HOST, PORT = '127.0.0.1', 8020
+	
+	server = MyTCPServer((HOST, PORT), MyRequestHandler)
+
+	server.serve_forever()
---
+++ utils/response.py
@@ -0,0 +1,68 @@
+from email.utils import formatdate
+
+class ResponseHandler():
+
+	StatusMapping = {'404' : 'Not Found', '400' : 'Bad Request', '200' : 'OK'}
+
+	
+	def __init__(self, request):
+		self.request =  request
+		self.HeaderTemplate = ''
+		self.ResponseLineTemplate = ''
+		self.HeaderDict = {}
+		#print("IN YOUR SUCKING CLASSES CTOR")
+		self.body = '<html><body><h1>Oh my god</h1></body></html>'
+		
+		#self.destination = requestSocket
+
+	def SetHeader(self, key, val):
+		self.HeaderDict[key] = val
+
+	def FillHeaderTemplate(self):
+		for k, v in self.HeaderDict.items():
+			self.HeaderTemplate = self.HeaderTemplate + k + ' : ' + v + '\r\n'
+
+	def FillResponseLineTemplate(self):
+		self.ResponseLineTemplate = 'HTTP/1.1 ' + self.StatusCode + ' ' + self.StatusMapping[self.StatusCode] + '\r\n'
+
+	def SendHeaders(self):
+		self.request.sendall(bytes(self.HeaderTemplate, 'utf-8'))
+
+	def EndHeaders(self):
+		self.request.sendall(bytes('\r\n', 'utf-8'))
+
+	def SendResponseLine(self):
+		self.request.sendall(bytes(self.ResponseLineTemplate, 'utf-8'))
+
+	def SendBody(self):
+		self.request.sendall(bytes(self.body, 'utf-8'))
+
+	def ParseRequest(self):
+		pass
+
+	# def FillErrorTemplate():
+
+	def MASTERHANDLER(self):
+		
+		self.StatusCode = '200'
+
+		self.SetHeader('Server', 'Python/0.1.0 (Custom)')
+		self.SetHeader('Content-Type', 'text/html')
+		self.SetHeader('Date', formatdate(timeval=None, localtime=False, usegmt=True))
+
+		self.FillResponseLineTemplate()
+		self.FillHeaderTemplate()
+
+		self.SendResponseLine()
+		self.SendHeaders()
+		self.EndHeaders()
+		self.SendBody()
+
+
+
+
+	
+
+
+
+
